1a42be042ec2a6e4bb9250b9d1e60dfc1ef1895b
etah <arrahetah23@gmail.com>
2024-07-17 19:50:45 +0100

    deleted svg files

--- src/hooks/use-current-user.tsx
+++ src/hooks/use-current-user.tsx
@@ -20,13 +20,3 @@ export const useCurrentUser = () => {
     user: session.data?.user,
   };
 };
-
-// export const useCurrentSession = (sessionId: string) => {
-//   const session = useSession();
-//   const user = session.data?.user;
-//   const userSessions = user.sessions;
-//   const currentSession = userSessions.find(
-//     (userSession: any) => userSession.id === sessionId
-//   );
-//   return { currentSession };
-// };
