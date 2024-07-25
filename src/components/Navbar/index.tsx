b95f90400488a1bad5407fd6c4989366c48e9e22
etah <arrahetah23@gmail.com>
2024-07-25 20:55:19 +0100

    adjustments made to the navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -37,108 +37,111 @@ const Navbar = () => {
     });
   };
   return (
-    <div className="bg-purple rounded-full m-4 p-2 z-50 fixed phone:w-[96%] flex items-center justify-between  w-[90]">
-      <Link
-        className="md:text-3xl font-semibold text-white p-2 text-lg sm:text-2xl cursor-pointer flex"
-        href={'/'}
-      >
-        <p className="text-lightPink">Accountability </p>
-        Tribe
-      </Link>
+    <div className=" mt-4 p-2 z-50 fixed flex items-center justify-center  w-full">
+      <div className="bg-purple rounded-full justify-between items-center flex w-[96%] mx-2">
+        <Link
+          className="md:text-3xl font-semibold text-white p-2 text-lg sm:text-2xl cursor-pointer flex"
+          href={'/'}
+        >
+          <p className="text-lightPink">Accountability </p>
+          Tribe
+        </Link>
 
-      <div className=" rounded-2xl p-2 lg:flex gap-2 items-center hidden">
-        <Link
-          href={'/tribes?page=1'}
-          className={cn(
-            'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl group',
-            pathname.startsWith('/tribes') && 'bg-black text-white shadow-none'
-          )}
-        >
-          <p className="w-full group-hover:text-white">Tribes</p>
-        </Link>
-        <Link
-          href={'/sessions?page=1&filter=all'}
-          className={cn(
-            'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl',
-            pathname.startsWith('/sessions') &&
-              'bg-black text-white shadow-none'
-          )}
-        >
-          Sessions
-        </Link>
-        {session.status === 'authenticated' && (
+        <div className=" rounded-2xl p-2 lg:flex gap-2 items-center hidden">
           <Link
-            href={'/tribe-admin-management'}
+            href={'/tribes?page=1'}
             className={cn(
-              'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl',
-              pathname.startsWith('/tribe-admin-management') &&
+              'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl group',
+              pathname.startsWith('/tribes') &&
                 'bg-black text-white shadow-none'
             )}
           >
-            Manage Tribes
+            <p className="w-full group-hover:text-white">Tribes</p>
           </Link>
-        )}
-      </div>
-      {session.status === 'authenticated' ? (
-        <div className="flex justify-between items-center gap-x-4">
-          <FaBars
-            size={25}
-            className="text-lightPink block lg:hidden "
-            onClick={() => setOpenDropdown((prev) => !prev)}
-          />
-          {openDropdown && (
-            <MobileView closeDropdown={() => setOpenDropdown(false)} />
-          )}{' '}
-          <div className="relative lg:flex items-center gap-4 hidden">
-            <StreakIcon count={user?.streak?.count} />
-            <NotificationIcon notifications={user.notifications} />
+          <Link
+            href={'/sessions?page=1&filter=all'}
+            className={cn(
+              'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl',
+              pathname.startsWith('/sessions') &&
+                'bg-black text-white shadow-none'
+            )}
+          >
+            Sessions
+          </Link>
+          {session.status === 'authenticated' && (
+            <Link
+              href={'/tribe-admin-management'}
+              className={cn(
+                'bg-lightPink rounded-2xl p-2 text-center text-xl uppercase hover:bg-black hover:text-white move-button text-black shadow-3xl',
+                pathname.startsWith('/tribe-admin-management') &&
+                  'bg-black text-white shadow-none'
+              )}
+            >
+              Manage Tribes
+            </Link>
+          )}
+        </div>
+        {session.status === 'authenticated' ? (
+          <div className="flex justify-between items-center gap-x-4">
+            <FaBars
+              size={25}
+              className="text-lightPink block lg:hidden "
+              onClick={() => setOpenDropdown((prev) => !prev)}
+            />
+            {openDropdown && (
+              <MobileView closeDropdown={() => setOpenDropdown(false)} />
+            )}{' '}
+            <div className="relative lg:flex items-center gap-4 hidden">
+              <StreakIcon count={user?.streak?.count} />
+              <NotificationIcon notifications={user.notifications} />
+            </div>
+            <ProfileIcon
+              deleteUser={
+                <Button
+                  variant={'destructive'}
+                  onClick={() => setOpenDeleteUserModal(true)}
+                >
+                  Delete Account
+                </Button>
+              }
+            />
           </div>
-          <ProfileIcon
-            deleteUser={
-              <Button
-                variant={'destructive'}
-                onClick={() => setOpenDeleteUserModal(true)}
-              >
-                Delete Account
+        ) : (
+          <div className="flex gap-2">
+            <Link
+              className="font-thin hover:font-bold cursor-pointer"
+              href={'/auth/login'}
+            >
+              Login
+            </Link>
+            <Link
+              href={'/auth/signup'}
+              className="text-white font-thin hover:font-bold cursor-pointer"
+            >
+              Sign Up
+            </Link>
+          </div>
+        )}
+        <ModalWrapper
+          isOpen={openDeleteUserModal}
+          onRequestClose={() => setOpenDeleteUserModal(false)}
+        >
+          <DeleteConfirmation
+            trigger={
+              <Button variant={'destructive'} disabled={isPending}>
+                Delete My Account
+              </Button>
+            }
+            confirmationMessage={'Are you sure you want to delete your account'}
+            consequenceMessage={'This action cannot be undone'}
+            action={
+              <Button variant={'destructive'} onClick={deleteAccount}>
+                Delete Anyway
               </Button>
             }
           />
-        </div>
-      ) : (
-        <div className="flex gap-2">
-          <Link
-            className="font-thin hover:font-bold cursor-pointer"
-            href={'/auth/login'}
-          >
-            Login
-          </Link>
-          <Link
-            href={'/auth/signup'}
-            className="text-white font-thin hover:font-bold cursor-pointer"
-          >
-            Sign Up
-          </Link>
-        </div>
-      )}
-      <ModalWrapper
-        isOpen={openDeleteUserModal}
-        onRequestClose={() => setOpenDeleteUserModal(false)}
-      >
-        <DeleteConfirmation
-          trigger={
-            <Button variant={'destructive'} disabled={isPending}>
-              Delete My Account
-            </Button>
-          }
-          confirmationMessage={'Are you sure you want to delete your account'}
-          consequenceMessage={'This action cannot be undone'}
-          action={
-            <Button variant={'destructive'} onClick={deleteAccount}>
-              Delete Anyway
-            </Button>
-          }
-        />
-      </ModalWrapper>
+        </ModalWrapper>
+      </div>
     </div>
   );
 };
