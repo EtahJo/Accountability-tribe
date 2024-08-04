85147b27a4603d0cabfc8953c110997a18cfea1e
etah <arrahetah23@gmail.com>
2024-08-04 05:39:19 +0100

    trial and error to fix aws amplify issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import HomePageLoggedIn from "@/components/HomePage/HomePageLoggedIn";
-import HomePageLoggedOut from "@/components/HomePage/HomePageLoggedOut";
-import { currentUser } from "@/lib/authentication";
-
-const base_url = process.env.BASE_URL;
+// import HomePageLoggedIn from "@/components/HomePage/HomePageLoggedIn";
+// import HomePageLoggedOut from "@/components/HomePage/HomePageLoggedOut";
+// import { currentUser } from "@/lib/authentication";
 
 const Home = async () => {
-	const user: any = await currentUser();
-	if (!user) return <HomePageLoggedOut />;
+	// const user: any = await currentUser();
+	// if (!user) return <HomePageLoggedOut />;
 	return (
 		<main className="">
-			<HomePageLoggedIn />
+			Hello world
+			{/* <HomePageLoggedIn /> */}
 		</main>
 	);
 };
