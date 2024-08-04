d7d240d8180dafe1a5f63f56079679ec161cde7b
etah <arrahetah23@gmail.com>
2024-08-04 06:13:22 +0100

    uncommented the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-// import HomePageLoggedIn from "@/components/HomePage/HomePageLoggedIn";
-// import HomePageLoggedOut from "@/components/HomePage/HomePageLoggedOut";
-// import { currentUser } from "@/lib/authentication";
+import HomePageLoggedIn from "@/components/HomePage/HomePageLoggedIn";
+import HomePageLoggedOut from "@/components/HomePage/HomePageLoggedOut";
+import { currentUser } from "@/lib/authentication";
 
 const Home = async () => {
-	// const user: any = await currentUser();
-	// if (!user) return <HomePageLoggedOut />;
+	const user: any = await currentUser();
+	if (!user) return <HomePageLoggedOut />;
 	return (
 		<main className="">
-			Hello world
-			{/* <HomePageLoggedIn /> */}
+			<HomePageLoggedIn />
 		</main>
 	);
 };
